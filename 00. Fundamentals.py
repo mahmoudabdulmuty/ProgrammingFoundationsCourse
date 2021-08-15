ef6b7c2894725484034c0f145c283a06ce652a6b
Mahmoud Abdulmuty <mahmoudabdulmuty@gmail.com>
2021-08-15 05:42:10 +0200

    first commit

---
+++ 00. Fundamentals.py
@@ -0,0 +1,20 @@
+# Guess Game Challenge
+answer = input("What's my favorite food?")
+
+if answer == 'pasta':
+    print('Yep! So Amazing')
+else:
+    print("Yuck! That's not it!")
+
+print("Thanks for playing!")
+
+# Favorite City Challenge
+
+
+def favorite_city(name):
+    print('one of my favorite cities is ' + name)
+
+
+favorite_city('Barcelona')
+favorite_city('London')
+favorite_city('Rome')
